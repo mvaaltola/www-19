b921eb1e468d6f42011c4aa713cb0b0c811601bb
Mikael Vaaltola <mvaaltola@gmail.com>
2019-04-15 22:56:45 +0300

    Serve and import local fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,13 @@ import React from "react"
 import styled from "styled-components"
 import Header from "./header"
 import Frame from "./frame"
+import "typeface-nobile"
+import "typeface-oxygen"
+import "typeface-raleway"
 
 const Wrapper = styled.div`
-  width: 100vw;
-  height: 100wh;
+  width: 100%;
+  height: 100%;
 `
 
 export default ({ children }) => (
