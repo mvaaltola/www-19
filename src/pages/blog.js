2a871a3322b83579a01a6e2695f3f8411a4436b4
Mikael Vaaltola <mvaaltola@gmail.com>
2019-04-17 00:11:23 +0300

    Show blog posts on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,42 @@
 import React from "react"
 import Layout from "../components/layout"
+import { Link, graphql } from "gatsby"
 
-export default () => (
-  <Layout>
-    <div>Hello world!</div>
-  </Layout>
-)
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link
+            to={node.fields.slug}>
+            <h3>{node.frontmatter.title}{" - "}<span>{node.frontmatter.date}</span></h3>
+          </Link>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD.MM.YYYY")
+          }
+          excerpt
+          html
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
